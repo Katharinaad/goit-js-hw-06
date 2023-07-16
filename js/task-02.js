8a19b776643d66d77ab9df99010233cffb41961a
Katharinaad <vermilion.adams@gmail.com>
2023-07-16 08:13:30 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,13 +1,13 @@
-// const ingredients = ["Potatoes", "Mushrooms", "Garlic", "Tomatos", "Herbs", "Condiments"];
+const ingredients = ["Potatoes", "Mushrooms", "Garlic", "Tomatos", "Herbs", "Condiments"];
 
-// const ingredientUl = document.querySelector("#ingredients");
+const ingredientUl = document.querySelector("#ingredients");
 
-// for (const ingredient of ingredients) {
-//   const ingredientLi = document.createElement("li");
-//   ingredientLi.textContent = ingredient;
-//   ingredientLi.classList.add = "item";
-//   ingredientUl.appendChild(ingredientLi);
-// }
-// document.body.appendChild(ingredientUl);
+for (const ingredient of ingredients) {
+  const ingredientLi = document.createElement("li");
+  ingredientLi.textContent = ingredient;
+  ingredientLi.classList.add = "item";
+  ingredientUl.appendChild(ingredientLi);
+}
+document.body.appendChild(ingredientUl);
 
-// console.log(ingredientUl);
+console.log(ingredientUl);
