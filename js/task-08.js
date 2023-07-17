7be15791e5b2dc07a710dd3f63ed457ee398d4ba
Katharinaad <vermilion.adams@gmail.com>
2023-07-17 11:31:28 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -5,6 +5,9 @@ form.addEventListener("submit", onSubmit);
 function onSubmit(event) {
   event.preventDefault();
   console.dir(form);
+
+  console.log(`Email: ${form.email.value} Password: ${form.password.value}`);
+
   if (form.email.value === "" || form.password.value === "") {
     alert("All fields must be filled in");
   }
