32fa76e3b2f420be4627540a260ca3ed8a59a912
Katharinaad <vermilion.adams@gmail.com>
2023-07-17 10:32:34 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,33 @@
+const inputNum = document.querySelector("input");
+console.log(inputNum);
+
+const createBtn = document.querySelector("button[data-create]");
+console.log(createBtn);
+
+const destroyBtn = document.querySelector("button[data-destroy]");
+console.log(destroyBtn);
+
+const divBoxes = document.getElementById("boxes");
+console.log(divBoxes);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+createBtn.addEventListener("click", function (amount) {
+  const inputValue = Number(inputNum.value);
+  console.log(inputValue);
+
+  let i = inputValue;
+
+  for (i = 0; i < amount; i += 1) {
+    const div = document.createElement("div");
+    div.className = "box";
+
+    divBoxes.appendChild(div);
+  }
+});
+
+destroyBtn.addEventListener("click", function () {});
